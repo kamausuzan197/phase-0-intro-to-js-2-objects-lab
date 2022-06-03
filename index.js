7c9e603c52cd02b928b7587e7a371aa17186a85c
kamausuzan197 <kamaususan197@gmail.com>
2022-06-03 07:43:39 +0300

    learning js objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: '',
+    streetAddress:''
+}
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+     const newEmployee = {...key};
+     delete employee.streetAddress;
+     return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key,value){
+    employee[key]=value;
+    return employee;
+
+}
